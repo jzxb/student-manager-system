43e61e375da2ca07a99b1b3119ba2362f46464e7
jzxb <qq807572809@live.com>
2019-11-02 20:36:34 +0800

    增加mapper功能

--- managersystem/src/main/java/com/suixingpay/student/mapper/StudentMapper.java
+++ managersystem/src/main/java/com/suixingpay/student/mapper/StudentMapper.java
@@ -1,6 +1,7 @@
 package com.suixingpay.student.mapper;
 
 import com.suixingpay.student.bean.Student;
+import com.suixingpay.student.expection.PcException;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,74 @@ import java.util.List;
 @Repository
 public interface StudentMapper {
 
-    boolean save(Student student);
+    /**
+     * 新增对象
+     * @author lhx
+     * @param student 新增对象
+     * @throws PcException 抛出的业务异常
+     */
+    void save(Student student) throws PcException;
 
-    List<Student> getStudentList();
+    /**
+     * 根据id更新对象
+     * @author lhx
+     * @param student 要更新的对象
+     * @throws PcException 抛出的业务异常
+     */
+    void updateById(Student student) throws PcException;
+
+    /**
+     * 根据id删除对象
+     * @author lhx
+     * @param id 要删除的id
+     * @throws PcException 抛出的业务异常
+     */
+    void deleteById(Integer id) throws PcException;
+
+    /**
+     * 根据id查找用户
+     * @author lhx
+     * @param id 要查找用户的id
+     * @return 查找到的用户
+     * @throws PcException 抛出的业务异常
+     */
+    Student findById(Integer id) throws PcException;
+
+    /**
+     * 根据条件查找用户
+     * @author lhx
+     * @param student 要查找的条件
+     * @return 查找到的用户列表
+     * @throws PcException 抛出的业务异常
+     */
+    List<Student> findByVO(Student student) throws PcException;
+
+    /**
+     * 查找所有的用户
+     * @author lhx
+     * @return 返回查到的所有用户
+     * @throws PcException 抛出的业务异常
+     */
+    List<Student> getStudentList() throws PcException;
+
+    /**
+     * 查找找到的第一条记录
+     * @author lhx
+     * @param student 要查找的条件
+     * @return 查找到的用户
+     * @throws PcException 抛出业务异常
+     */
+    Student findFirst(Student student) throws PcException;
+
+    /**
+     * 模糊查询
+     * @author lhx
+     * @param student 模糊查询条件
+     * @return 查询到的用户
+     * @throws PcException 抛出业务异常
+     */
+    List<Student> findLikeVO(Student student) throws PcException;
+
+
+
 }
