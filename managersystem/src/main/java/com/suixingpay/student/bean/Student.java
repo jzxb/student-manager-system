afcdfb548ccdcae913c195effc42ee7fb9e58a81
jzxb <qq807572809@live.com>
2019-11-02 21:18:20 +0800

    修改字段

--- managersystem/src/main/java/com/suixingpay/student/bean/Student.java
+++ managersystem/src/main/java/com/suixingpay/student/bean/Student.java
@@ -5,13 +5,22 @@ package com.suixingpay.student.bean;
  * @date 2019/11/2 - 20:41
  */
 public class Student {
+    private String id;
     private String numberId;
     private String name;
-    private String sex;
+    private String gender;
     private String age;
     private String major;
     private String grade;
     private String phoneNumber;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getNumberId() {
         return numberId;
@@ -21,8 +30,8 @@ public class Student {
         return name;
     }
 
-    public String getSex() {
-        return sex;
+    public String getGender() {
+        return gender;
     }
 
     public String getAge() {
@@ -49,8 +58,8 @@ public class Student {
         this.name = name;
     }
 
-    public void setSex(String sex) {
-        this.sex = sex;
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public void setAge(String age) {
@@ -74,7 +83,7 @@ public class Student {
         return "Student{" +
                 "numberId='" + numberId + '\'' +
                 ", name='" + name + '\'' +
-                ", sex='" + sex + '\'' +
+                ", gender='" + gender + '\'' +
                 ", age='" + age + '\'' +
                 ", major='" + major + '\'' +
                 ", grade='" + grade + '\'' +
