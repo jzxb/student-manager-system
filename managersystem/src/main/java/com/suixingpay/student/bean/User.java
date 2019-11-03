a64df24f99a4d416c9a898aac2cea3511ff32e8e
liaoaoao <709147360@qq.com>
2019-11-03 10:21:15 +0800

    增加User类

---
+++ managersystem/src/main/java/com/suixingpay/student/bean/User.java
@@ -0,0 +1,29 @@
+package com.suixingpay.student.bean;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author lichanghao
+ * @date 2019/11/3 - 10:08
+ */
+
+
+@Getter
+@Setter
+public class User {
+    private int id;
+    private String userName;
+    private String passWord;
+
+
+    @Override
+    public String toString(){
+        return "User{" +
+                "Id='" + id + '\'' +
+                ", userName='" + userName + '\'' +
+                ", passWord='" + passWord + '\'' +
+                '}';
+    }
+}
