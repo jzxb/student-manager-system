8c9f8fa4d28e53392cd57472abaaeae7caa2a888
Mrs.yao <s17745134408@163.com>
2019-11-03 14:35:46 +0800

    更改一下XML

--- managersystem/src/main/java/com/suixingpay/student/bean/User.java
+++ managersystem/src/main/java/com/suixingpay/student/bean/User.java
@@ -17,6 +17,29 @@ public class User {
     private String userName;
     private String passWord;
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
 
     @Override
     public String toString(){
