14e2b68660061d4b43ee22faf0e633f0be7c1a42
jzxb <qq807572809@live.com>
2019-11-03 15:12:33 +0800

    修改字段

--- managersystem/src/main/java/com/suixingpay/student/bean/Student.java
+++ managersystem/src/main/java/com/suixingpay/student/bean/Student.java
@@ -5,8 +5,7 @@ package com.suixingpay.student.bean;
  * @date 2019/11/2 - 20:41
  */
 public class Student {
-    private String id;
-    private String numberId;
+    private int numberId;
     private String name;
     private String gender;
     private String age;
@@ -14,64 +13,57 @@ public class Student {
     private String grade;
     private String phoneNumber;
 
-    public String getId() {
-        return id;
-    }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNumberId() {
+    public int getNumberId() {
         return numberId;
+    }
+
+    public void setNumberId(int numberId) {
+        this.numberId = numberId;
     }
 
     public String getName() {
         return name;
     }
 
-    public String getGender() {
-        return gender;
-    }
-
-    public String getAge() {
-        return age;
-    }
-
-    public String getMajor() {
-        return major;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setNumberId(String numberId) {
-        this.numberId = numberId;
-    }
-
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
     }
 
     public void setGender(String gender) {
         this.gender = gender;
     }
 
+    public String getAge() {
+        return age;
+    }
+
     public void setAge(String age) {
         this.age = age;
+    }
+
+    public String getMajor() {
+        return major;
     }
 
     public void setMajor(String major) {
         this.major = major;
     }
 
+    public String getGrade() {
+        return grade;
+    }
+
     public void setGrade(String grade) {
         this.grade = grade;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
     }
 
     public void setPhoneNumber(String phoneNumber) {
